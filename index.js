3a75d90c0e1c1de3fc1c73a829f013600c1a7660
rose-okoth <okoth.rose0@gmail.com>
2021-01-25 22:53:04 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+class Car {
+    gearbox;
+    wheels;
+    seats;
+    windows;
+    side_mirrors;
+    engine;
+    clutch;
+    brake;
+
+    constructor(gearbox, wheels, seats, windows, side_mirrors, engine, clutch, brake) {
+        this.gearbox = gearbox;
+        this.wheels = wheels;
+        this.seats = seats;
+        this.windows = windows;
+        this.side_mirrors = side_mirrors;
+        this.engine = engine;
+        this.clutch = clutch;
+        this.brake = brake;
+    }
+}
